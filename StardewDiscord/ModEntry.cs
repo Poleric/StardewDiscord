feee7d6731ca74aec6293ac7879ff140fada1b87
Steven Kraft <steven.kraftjp@gmail.com>
2019-03-08 21:36:07 -0500

    Version Update and Small Cleanup

--- StardewDiscord/ModEntry.cs
+++ StardewDiscord/ModEntry.cs
@@ -14,9 +14,6 @@ namespace StardewDiscord
     /// <summary>The mod entry point.</summary>
     public class ModEntry : Mod
     {
-        /*********
-        ** Public methods
-        *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
